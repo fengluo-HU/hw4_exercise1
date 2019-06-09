09fa6dfa2c42840b41b4e29786729b367a75f326
fengluo-HU <fluo@my.harrisburgu.edu>
2019-06-09 07:27:05 -0400

    update message

--- src/main/java/com/drkiettran/microprofile/rest/HelloWorldEndpoint.java
+++ src/main/java/com/drkiettran/microprofile/rest/HelloWorldEndpoint.java
@@ -1,6 +1,9 @@
 package com.drkiettran.microprofile.rest;
 
 import java.net.UnknownHostException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -17,12 +20,15 @@ public class HelloWorldEndpoint {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("{year}/{month}/{day}")
-	public Response doGetDate(@PathParam("year") int year, @PathParam("month") int month, @PathParam("day") int day) {
+	public Response doGetDate(@PathParam("year") int year, @PathParam("month") int month, @PathParam("day") int day)
+			throws ParseException {
 
 		String date = year + "/" + month + "/" + day;
 		Message message = new Message();
 		message.setName("Thorntail");
 		message.setMessage(date);
+		Date date1= new SimpleDateFormat("yyyy/MM/dd").parse(date);
+		message.setDate(date1);
 		return Response.status(200).entity(message).build();
 	}
 
@@ -31,7 +37,7 @@ public class HelloWorldEndpoint {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/{year}/{month}/{day}")
 	public Response doPutDate(@PathParam("year") int year, @PathParam("month") int month, @PathParam("day") int day,
-			Message inMessage) throws UnknownHostException {
+			Message inMessage) throws UnknownHostException, ParseException {
 		System.out.println("inMessage: " + inMessage.getName() + ":" + inMessage.getMessage());
 
 		String date = year + "/" + month + "/" + day;
@@ -39,6 +45,8 @@ public class HelloWorldEndpoint {
 		Greetings greetings = new Greetings();
 		message.setName(inMessage.getName());
 		message.setMessage(greetings.hello(inMessage.getName()));
+		Date date1= new SimpleDateFormat("yyyy/MM/dd").parse(date);
+		message.setDate(date1);
 		return Response.status(200).entity(message).build();
 	}
 }
